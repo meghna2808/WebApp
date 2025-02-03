5a18d3b54924f8dfd4fcf77612613fff9c6a4ccc
meghnas2808 <meghnasingh2808@gmail.com>
2025-02-03 23:17:32 +0530

    added controller classses

---
+++ src/main/java/com/example/webApp/AboutController.java
@@ -0,0 +1,12 @@
+package com.example.webApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AboutController {
+    @RequestMapping("/about")
+    public String about(){
+        return "My name is Meghna. I am a software engineer";
+    }
+}
